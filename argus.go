d137125a7df28b167a0d1b1c99f8815a70278e31
Nasr-sys <676ashoaq12@gmail.com>
2026-03-30 15:39:47 +0300

    Update argus.go

--- argus.go
+++ argus.go
@@ -1,12 +1,14 @@
 package medosigner
 
 import (
-	"crypto/md5"
-	"crypto/rand"
-	"encoding/base64"
-	"encoding/binary"
-	"math"
-	"time"
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/md5"
+    "crypto/rand"
+    "encoding/base64"
+    "encoding/binary"
+    "fmt"
+    "time"
 )
 
 type Argus struct{}
@@ -75,61 +77,66 @@ func hexDecode(s string) ([]byte, error) {
 }
 
 func (a *Argus) Encrypt(xargusBean map[int]interface{}) (string, error) {
-	pb, err := NewProtoBuf(xargusBean)
-	if err != nil {
-		return "", err
-	}
-	protobuf := pb.ToBuf()
-	// PKCS7 padding
-	paddedLen := ((len(protobuf) + 15) / 16) * 16
-	padded := make([]byte, paddedLen)
-	copy(padded, protobuf)
-	PKCS7PaddingPadBuffer(padded, len(protobuf), paddedLen, 16)
+    pb, err := NewProtoBuf(xargusBean)
+    if err != nil {
+        return "", err
+    }
+    protobuf := pb.ToBuf()
+    // PKCS7 padding
+    paddedLen := ((len(protobuf) + 15) / 16) * 16
+    padded := make([]byte, paddedLen)
+    copy(padded, protobuf)
+    PKCS7PaddingPadBuffer(padded, len(protobuf), paddedLen, 16)
 
-	newLen := len(padded)
+    newLen := len(padded)
 
-	signKey := []byte("\xac\x1a\xda\xae\x95\xa7\xaf\x94\xa5\x11J\xb3\xb3\xa9}\xd8\x00P\xaa\n91L@R\x8c\xae\xc9RV\xc2\x8c")
-	// sm3_hash(sign_key + b'\xf2\x81ao' + sign_key)
-	sm3Hash := []byte("\xfcx\xe0\xa9ez\x0ct\x8c\xe5\x15Y\x90<\xcf\x03Q\x0eQ\xd3\xcf\xf22\xd7\x13C\xe8\x8a2\x1cS\x04")
+    signKey := []byte("\xac\x1a\xda\xae\x95\xa7\xaf\x94\xa5\x11J\xb3\xb3\xa9}\xd8\x00P\xaa\n91L@R\x8c\xae\xc9RV\xc2\x8c")
+    // sm3_hash(sign_key + b'\xf2\x81ao' + sign_key)
+    sm3Hash := []byte("\xfcx\xe0\xa9ez\x0ct\x8c\xe5\x15Y\x90<\xcf\x03Q\x0eQ\xd3\xcf\xf22\xd7\x13C\xe8\x8a2\x1cS\x04")
 
-	key := sm3Hash[:32]
-	keyList := make([]uint64, 4)
-	for i := 0; i < 2; i++ {
-		keyList[i*2] = binary.LittleEndian.Uint64(key[i*16 : i*16+8])
-		keyList[i*2+1] = binary.LittleEndian.Uint64(key[i*16+8 : i*16+16])
-	}
+    key := sm3Hash[:32]
+    keyList := make([]uint64, 4)
+    for i := 0; i < 2; i++ {
+        keyList[i*2] = binary.LittleEndian.Uint64(key[i*16 : i*16+8])
+        keyList[i*2+1] = binary.LittleEndian.Uint64(key[i*16+8 : i*16+16])
+    }
 
-	encPb := make([]byte, newLen)
-	for i := 0; i < newLen/16; i++ {
-		pt := make([]uint64, 2)
-		pt[0] = binary.LittleEndian.Uint64(padded[i*16 : i*16+8])
-		pt[1] = binary.LittleEndian.Uint64(padded[i*16+8 : i*16+16])
-		ct := SimonEnc(pt, keyList, 0)
-		binary.LittleEndian.PutUint64(encPb[i*16:], ct[0])
-		binary.LittleEndian.PutUint64(encPb[i*16+8:], ct[1])
-	}
+    encPb := make([]byte, newLen)
+    for i := 0; i < newLen/16; i++ {
+        pt := make([]uint64, 2)
+        pt[0] = binary.LittleEndian.Uint64(padded[i*16 : i*16+8])
+        pt[1] = binary.LittleEndian.Uint64(padded[i*16+8 : i*16+16])
+        ct := SimonEnc(pt, keyList, 0)
+        binary.LittleEndian.PutUint64(encPb[i*16:], ct[0])
+        binary.LittleEndian.PutUint64(encPb[i*16+8:], ct[1])
+    }
 
-	buffer := make([]byte, newLen+8)
-	copy(buffer[:8], []byte("\xf2\xf7\xfc\xff\xf2\xf7\xfc\xff"))
-	copy(buffer[8:], encPb)
+    buffer := make([]byte, newLen+8)
+    copy(buffer[:8], []byte("\xf2\xf7\xfc\xff\xf2\xf7\xfc\xff"))
+    copy(buffer[8:], encPb)
 
-	bBuffer := a.encryptEncPb(buffer, newLen+8)
-	bBuffer = append([]byte("\xa6n\xad\x9fw\x01\xd0\x0c\x18"), bBuffer...)
-	bBuffer = append(bBuffer, []byte("ao")...)
+    bBuffer := a.encryptEncPb(buffer, newLen+8)
+    bBuffer = append([]byte("\xa6n\xad\x9fw\x01\xd0\x0c\x18"), bBuffer...)
+    bBuffer = append(bBuffer, []byte("ao")...)
 
-	md5Hash := md5.Sum(signKey[:16])
-	cipher, err := NewAESCBC(md5Hash[:], md5.Sum(signKey[16:])[:])
-	if err != nil {
-		return "", err
-	}
+    // إصلاح الخطأ هنا
+    md5Hash := md5.Sum(signKey[:16])
+    md5Key := md5.Sum(signKey[16:])
+    cipher, err := NewAESCBC(md5Hash[:], md5Key[:])
+    if err != nil {
+        return "", err
+    }
 
-	paddedBBuffer := make([]byte, ((len(bBuffer)+15)/16)*16)
-	copy(paddedBBuffer, bBuffer)
-	PKCS7PaddingPadBuffer(paddedBBuffer, len(bBuffer), len(paddedBBuffer), 16)
+    paddedBBuffer := make([]byte, ((len(bBuffer)+15)/16)*16)
+    copy(paddedBBuffer, bBuffer)
+    PKCS7PaddingPadBuffer(paddedBBuffer, len(bBuffer), len(paddedBBuffer), 16)
 
-	encrypted := cipher.Encrypt(paddedBBuffer)
-	result := append([]byte("\xf2\x81"), encrypted...)
-	return base64.StdEncoding.EncodeToString(result), nil
+    encrypted, err := cipher.Encrypt(paddedBBuffer)
+    if err != nil {
+        return "", err
+    }
+    result := append([]byte("\xf2\x81"), encrypted...)
+    return base64.StdEncoding.EncodeToString(result), nil
 }
 
 type AESCBC struct {
@@ -141,10 +148,21 @@ func NewAESCBC(key, iv []byte) (*AESCBC, error) {
 	return &AESCBC{key: key, iv: iv}, nil
 }
 
-func (c *AESCBC) Encrypt(data []byte) []byte {
-	// Simplified AES CBC - in production use crypto/aes
-	// This is a placeholder - actual implementation would use crypto/aes
-	return data
+func (c *AESCBC) Encrypt(data []byte) ([]byte, error) {
+    block, err := aes.NewCipher(c.key)
+    if err != nil {
+        return nil, err
+    }
+    
+    if len(data)%aes.BlockSize != 0 {
+        return nil, fmt.Errorf("data is not padded")
+    }
+    
+    mode := cipher.NewCBCEncrypter(block, c.iv)
+    encrypted := make([]byte, len(data))
+    mode.CryptBlocks(encrypted, data)
+    
+    return encrypted, nil
 }
 
 func (a *Argus) GetSign(queryhash string, data string, timestamp int64, aid int, licenseId int, platform int, secDeviceId string, sdkVersion string, sdkVersionInt int) (string, error) {
