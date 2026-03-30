9e4581fa3f2915a4fb7f57659810d6569c3056c2
Nasr-sys <676ashoaq12@gmail.com>
2026-03-30 17:17:21 +0300

    Update argus.go

--- argus.go
+++ argus.go
@@ -8,72 +8,73 @@ import (
     "encoding/base64"
     "encoding/binary"
     "fmt"
+    "strconv"
     "time"
 )
 
 type Argus struct{}
 
 func (a *Argus) encryptEncPb(data []byte, l int) []byte {
-	xorArray := make([]byte, 8)
-	copy(xorArray, data[:8])
+    xorArray := make([]byte, 8)
+    copy(xorArray, data[:8])
 
-	result := make([]byte, l)
-	copy(result, data)
+    result := make([]byte, l)
+    copy(result, data)
 
-	for i := 8; i < l; i++ {
-		result[i] = data[i] ^ xorArray[i%8]
-	}
+    for i := 8; i < l; i++ {
+        result[i] = data[i] ^ xorArray[i%8]
+    }
 
-	// reverse
-	for i, j := 0, l-1; i < j; i, j = i+1, j-1 {
-		result[i], result[j] = result[j], result[i]
-	}
-	return result
+    // reverse
+    for i, j := 0, l-1; i < j; i, j = i+1, j-1 {
+        result[i], result[j] = result[j], result[i]
+    }
+    return result
 }
 
 func (a *Argus) GetBodyHash(stub string) []byte {
-	if stub == "" {
-		sm3 := NewSM3()
-		hash := sm3.Sm3Hash(make([]byte, 16))
-		return hash[:6]
-	}
-	sm3 := NewSM3()
-	data, _ := hexDecode(stub)
-	hash := sm3.Sm3Hash(data)
-	return hash[:6]
+    if stub == "" {
+        sm3 := NewSM3()
+        hash := sm3.Sm3Hash(make([]byte, 16))
+        return hash[:6]
+    }
+    sm3 := NewSM3()
+    data, _ := hexDecode(stub)
+    hash := sm3.Sm3Hash(data)
+    return hash[:6]
 }
 
 func (a *Argus) GetQueryHash(query string) []byte {
-	if query == "" {
-		sm3 := NewSM3()
-		hash := sm3.Sm3Hash(make([]byte, 16))
-		return hash[:6]
-	}
-	sm3 := NewSM3()
-	hash := sm3.Sm3Hash([]byte(query))
-	return hash[:6]
+    if query == "" {
+        sm3 := NewSM3()
+        hash := sm3.Sm3Hash(make([]byte, 16))
+        return hash[:6]
+    }
+    sm3 := NewSM3()
+    hash := sm3.Sm3Hash([]byte(query))
+    return hash[:6]
 }
 
 func hexDecode(s string) ([]byte, error) {
-	if len(s)%2 != 0 {
-		s = "0" + s
-	}
-	result := make([]byte, len(s)/2)
-	for i := 0; i < len(s); i += 2 {
-		var b byte
-		for j := 0; j < 2; j++ {
-			c := s[i+j]
-			if c >= '0' && c <= '9' {
-				b = (b << 4) | (c - '0')
-			} else if c >= 'a' && c <= 'f' {
-				b = (b << 4) | (c - 'a' + 10)
-			} else if c >= 'A' && c <= 'F' {
-				b = (b << 4) | (c - 'A' + 10)
-			}
-		}
-		result[i/2] = b
-	}
-	return result, nil
+    if len(s)%2 != 0 {
+        s = "0" + s
+    }
+    result := make([]byte, len(s)/2)
+    for i := 0; i < len(s); i += 2 {
+        var b byte
+        for j := 0; j < 2; j++ {
+            c := s[i+j]
+            if c >= '0' && c <= '9' {
+                b = (b << 4) | (c - '0')
+            } else if c >= 'a' && c <= 'f' {
+                b = (b << 4) | (c - 'a' + 10)
+            } else if c >= 'A' && c <= 'F' {
+                b = (b << 4) | (c - 'A' + 10)
+            }
+        }
+        result[i/2] = b
+    }
+    return result, nil
 }
 
 func (a *Argus) Encrypt(xargusBean map[int]interface{}) (string, error) {
@@ -119,7 +120,6 @@ func (a *Argus) Encrypt(xargusBean map[int]interface{}) (string, error) {
     bBuffer = append([]byte("\xa6n\xad\x9fw\x01\xd0\x0c\x18"), bBuffer...)
     bBuffer = append(bBuffer, []byte("ao")...)
 
-    // إصلاح الخطأ هنا
     md5Hash := md5.Sum(signKey[:16])
     md5Key := md5.Sum(signKey[16:])
     cipher, err := NewAESCBC(md5Hash[:], md5Key[:])
@@ -140,12 +140,12 @@ func (a *Argus) Encrypt(xargusBean map[int]interface{}) (string, error) {
 }
 
 type AESCBC struct {
-	key []byte
-	iv  []byte
+    key []byte
+    iv  []byte
 }
 
 func NewAESCBC(key, iv []byte) (*AESCBC, error) {
-	return &AESCBC{key: key, iv: iv}, nil
+    return &AESCBC{key: key, iv: iv}, nil
 }
 
 func (c *AESCBC) Encrypt(data []byte) ([]byte, error) {
@@ -153,60 +153,60 @@ func (c *AESCBC) Encrypt(data []byte) ([]byte, error) {
     if err != nil {
         return nil, err
     }
-    
+
     if len(data)%aes.BlockSize != 0 {
         return nil, fmt.Errorf("data is not padded")
     }
-    
+
     mode := cipher.NewCBCEncrypter(block, c.iv)
     encrypted := make([]byte, len(data))
     mode.CryptBlocks(encrypted, data)
-    
+
     return encrypted, nil
 }
 
 func (a *Argus) GetSign(queryhash string, data string, timestamp int64, aid int, licenseId int, platform int, secDeviceId string, sdkVersion string, sdkVersionInt int) (string, error) {
-	if timestamp == 0 {
-		timestamp = time.Now().Unix()
-	}
+    if timestamp == 0 {
+        timestamp = time.Now().Unix()
+    }
 
-	xargusBean := map[int]interface{}{
-		1:  0x20200929 << 1,
-		2:  2,
-		3:  randInt(0, 0x7FFFFFFF),
-		4:  aid,
-		5:  "device_id_placeholder",
-		6:  licenseId,
-		7:  "version_name_placeholder",
-		8:  sdkVersion,
-		9:  sdkVersionInt,
-		10: make([]byte, 8),
-		11: platform,
-		12: timestamp << 1,
-		13: a.GetBodyHash(data),
-		14: a.GetQueryHash(queryhash),
-		15: map[int]interface{}{
-			1: 1,
-			2: 1,
-			3: 1,
-			7: 3348294860,
-		},
-		16: secDeviceId,
-		20: "none",
-		21: 738,
-		23: map[int]interface{}{
-			1: "NX551J",
-			2: 8196,
-			4: 2162219008,
-		},
-		25: 2,
-	}
+    xargusBean := map[int]interface{}{
+        1:  uint64(0x20200929 << 1),
+        2:  uint64(2),
+        3:  uint64(randInt(0, 0x7FFFFFFF)),
+        4:  strconv.Itoa(aid),
+        5:  "device_id_placeholder",
+        6:  strconv.Itoa(licenseId),
+        7:  "version_name_placeholder",
+        8:  sdkVersion,
+        9:  uint64(sdkVersionInt),
+        10: make([]byte, 8),
+        11: uint64(platform),
+        12: uint64(timestamp << 1),
+        13: a.GetBodyHash(data),
+        14: a.GetQueryHash(queryhash),
+        15: map[int]interface{}{
+            1: uint64(1),
+            2: uint64(1),
+            3: uint64(1),
+            7: uint64(3348294860),
+        },
+        16: secDeviceId,
+        20: "none",
+        21: uint64(738),
+        23: map[int]interface{}{
+            1: "NX551J",
+            2: uint64(8196),
+            4: uint64(2162219008),
+        },
+        25: uint64(2),
+    }
 
-	return a.Encrypt(xargusBean)
+    return a.Encrypt(xargusBean)
 }
 
 func randInt(min, max int) int {
-	b := make([]byte, 4)
-	rand.Read(b)
-	return min + int(binary.BigEndian.Uint32(b))%(max-min)
+    b := make([]byte, 4)
+    rand.Read(b)
+    return min + int(binary.BigEndian.Uint32(b))%(max-min)
 }
